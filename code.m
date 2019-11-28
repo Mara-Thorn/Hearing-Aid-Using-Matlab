2a944057f7bd1d4490cbb7446657218ffb1ea224
Aayush Gupta <aayush17125@iiitd.ac.in>
2019-11-28 21:35:34 +0530

    fixed graphs

--- code.m
+++ code.m
@@ -20,10 +20,12 @@ load handel.mat
 fs=Fs;
 y = y(:, 1);
 %info = audioinfo(input);
+sound(y);
+pause(10);
 figure,plot(y);
 title('input');
+%%
 y = awgn(y,40);
-
 figure,plot(y);
 title('awgn');
 disp('playing added noise...');
@@ -33,7 +35,7 @@ pause(10)
 % yd = wdenoise(y,3,'Wavelet','db3','DenoisingMethod','UniversalThreshold','ThresholdRule','Soft','NoiseEstimate','LevelDependent');
 disp('playing denoised sound...');
 [thr,sorh,keepapp]=ddencmp( 'den' , 'wv' ,y);  
-yd=wdencmp( 'gbl' ,y, 'db3' ,2,thr,sorh,keepapp);  
+[yd,~,~,~,~]=wdencmp( 'gbl' ,y, 'db3' ,2,thr,sorh,keepapp);  
 sound(yd);
 
 figure,plot(yd);
@@ -63,15 +65,15 @@ gain = gain.';
 %for first stage
 firstC = (0.3*(g-1))/freqP(1);
 k = 0;
-while (k<=freqP(1)*N/fs)
+while (k/N<=freqP(1)/fs)
    gain(k+1) = firstC*k/(N*T) + 1; %paper step 1
-    gain(N-k) = gain(k+1);
-    k=k+1;
+   gain(N-k) = gain(k+1);
+   k=k+1;
 end
 % for second stage
 secondC = firstC*freqP(1)+1;
 secondC2 = (freqP(2)-freqP(1))/5;
-while (k<= freqP(2)*N/fs)
+while (k/N<= freqP(2)/fs)
     gain(k+1) = ((secondC-1)*exp(-((k/(N*T))-freqP(1))/secondC2)) + 1;
     gain(N-k) = gain(k+1);
     k=k+1;
@@ -79,32 +81,32 @@ end
 
 thirdC = ((secondC-1)*exp(-freqP(2)/secondC2)) +1;
 thirdC2 = (freqP(3)-freqP(2))/5;
-while (k<= freqP(3)*N/fs)
+while (k/N<= freqP(3)/fs)
     gain(k+1) = ((thirdC-g)*exp(-((k/(N*T)-freqP(2)))/thirdC2)) +g;
     gain(N-k) = gain(k+1);
     k=k+1;
 end
 
-while (k<=freqP(4)*N/fs)
+while (k/N<=freqP(4)/fs)
     gain(k+1) = g;
-    gain(N-k) = g;
+    gain(N-k) = gain(k+1);
     k=k+1;
 end
 
 fifthC = g;
 fifthC2 = (fs/2-freqP(4))/5;
-while (k<=N/2)
+while (k/N<=0.5)
     gain(k+1) = ((fifthC-1)*exp(-((k/(N*T))-freqP(4))/fifthC2)) +1;
     gain(N-k) = gain(k+1);
     k=k+1;
 end
 k_v = (0:N-1)/N;
+disp(N);
 figure,plot(k_v,gain);
 title('Gain');
-
 k_v = k_v*fs;
 k_v = k_v(1:N/2+1);
-plot(k_v,gain(1:N/2+1));
+figure,plot(k_v,gain(1:N/2+1));
 title('Frequency Shaper Transfer Function');
 xlabel('Frequency (Hertz)');
 ylabel('Gain');
